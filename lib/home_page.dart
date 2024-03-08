ebe2ec496e44447cd30a25266e197cfdf2482755
nishan_kenland <nishan.rasheed@kenland.in>
2024-03-08 16:05:38 +0530

    image size changed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,33 +11,23 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.amber,
       appBar: AppBar(title: const Text('Canvas skit'),),
-      body: Image(
-        height: h,width: w,
-        image: ResizeImage(
-        const AssetImage(AppAssets.bgImage,),
-        width: w.toInt(),height: h.toInt(),
-        ),
-        fit: BoxFit.cover,
-        ),
+      // body: Image(
+      //   height: h,width: w,
+      //   image: ResizeImage(
+      //   const AssetImage(AppAssets.bgImage,),
+      //   width: w.toInt(),height: h.toInt(),
+      //   ),
+      //   fit: BoxFit.cover,
+      //   ),
       // body: Image.asset(AppAssets.secondImage,
       //       cacheHeight: h.toInt(),
       //       cacheWidth: w.toInt(),
       //       fit: BoxFit.fill,
       // )
-      // Column(
-      //   children: [
-      //     const Text('image one'),
-      //     Expanded(child: ColoredBox(color: Colors.amber,
-      //       child: Image.asset(AppAssets.bgImage,
-      //       cacheHeight: h.toInt(),
-      //       cacheWidth: w.toInt(),
-      //       fit: BoxFit.contain,
-      //       ),
-      //     )),
-      //     const Text('image second'),
-      //     Expanded(child: Image.asset(AppAssets.secondImage,width: w,)),
-      //   ],
-      // ),
+      body: Image.asset(AppAssets.bgImage,
+      height: h,width: w,
+      fit: BoxFit.cover,
+      ),
     );
   }
 }
